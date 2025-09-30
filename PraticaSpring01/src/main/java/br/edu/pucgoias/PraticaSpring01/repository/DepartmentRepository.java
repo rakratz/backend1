897f02ec93527bbe70a46771ac2a19ca71a1b492
Prof. Me. Ricardo de Andrade Kratz <rakratz@gmail.com>
2025-09-30 20:16:05 -0300

    Delete PraticaSpring01 directory

--- PraticaSpring01/src/main/java/br/edu/pucgoias/PraticaSpring01/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.pucgoias.PraticaSpring01.repository;
-
-import br.edu.pucgoias.PraticaSpring01.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> { 
-	
-	
-}
--- PraticaSpring01/src/main/java/br/edu/pucgoias/PraticaSpring01/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.pucgoias.PraticaSpring01.repository;
-
-import br.edu.pucgoias.PraticaSpring01.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByDepartment_Name(String name); 
-}
